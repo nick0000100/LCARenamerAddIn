5d968970ccfcf91ae83b2aa5e74ea4fea6a0ca3c
Nicholas Sor (Robert Half International Inc) <v-nisor@microsoft.com>
2019-06-24 07:38:29 -0700

    initial commit, reads the subject of the emails

---
+++ Ribbon1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace LCARenamerAddIn
+{
+    partial class TestRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public TestRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.TestGroup = this.Factory.CreateRibbonGroup();
+            this.TestButton = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.TestGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabMail";
+            this.tab1.Groups.Add(this.TestGroup);
+            this.tab1.Label = "TabMail";
+            this.tab1.Name = "tab1";
+            // 
+            // TestGroup
+            // 
+            this.TestGroup.Items.Add(this.TestButton);
+            this.TestGroup.Label = "TestGroup";
+            this.TestGroup.Name = "TestGroup";
+            // 
+            // TestButton
+            // 
+            this.TestButton.Label = "TestButton";
+            this.TestButton.Name = "TestButton";
+            this.TestButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.TestButton_Click);
+            // 
+            // TestRibbon
+            // 
+            this.Name = "TestRibbon";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.TestGroup.ResumeLayout(false);
+            this.TestGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup TestGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton TestButton;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal TestRibbon Ribbon1
+        {
+            get { return this.GetRibbon<TestRibbon>(); }
+        }
+    }
+}
---
+++ Ribbon1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Outlook;
+using Microsoft.Office.Tools.Ribbon;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace LCARenamerAddIn
+{
+    public partial class TestRibbon
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        { 
+
+        }
+
+        private void TestButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            Outlook.Explorer explorer = Globals.ThisAddIn.Application.ActiveExplorer();
+            if (explorer != null)
+            {
+                List<String> LCANumbers = new List<String>();
+                foreach (MailItem email in new Microsoft.Office.Interop.Outlook.Application().ActiveExplorer().Selection)
+                {
+                    Match Match = Regex.Match(email.Subject, @"I[0-9\-]*$");
+                    System.Windows.Forms.MessageBox.Show(Match.Groups[0].Value);
+                    String LCANumber = Match.Groups[0].Value;
+                    LCANumbers.Add(LCANumber);
+
+                }
+            }
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if(fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show(fbd.SelectedPath);
+            }
+        }
+    }
+}
